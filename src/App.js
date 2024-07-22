ab0d33a70999fd012dec36abbdd91a19851156e1
Imasha Senadheera <imashasenadheera326@gmail.com>
2024-07-22 22:11:27 +0530

    14th Commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import SpecialsSection from "./components/SpecialsSection";
 import AboutUs from "./components/AboutUs";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import ProductsPage from "./components/ProductsPage"; // Import ProductsPage component
 
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
       <Header />
       <HeroSection id="home" />
       <SpecialsSection id="specials-section" />
-      <ProductsPage id="products-page"/>
       <AboutUs id="about-section" />
       <Contact id="contact-section" />
       <Footer />
